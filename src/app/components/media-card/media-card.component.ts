888868693941750cc551b9ac0aa4f77a03c40581
vhmi <vhmizq@gmail.com>
2024-09-16 21:43:09 -0600

    add components scaffolding

---
+++ src/app/components/media-card/media-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './media-card.component.html',
+  styleUrl: './media-card.component.scss'
+})
+export class MediaCardComponent {
+
+}
